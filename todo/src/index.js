e1331b5871c2ad9d5e1cbc9b026d473703752247
Manish Kusharee <manish.altcampus@gmail.com>
2022-02-04 17:28:26 +0530

    todo app using react redux

---
+++ todo/src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import reactDOM from 'react-dom';
+import App from './components/App';
+import { store } from './store';
+import { Provider } from 'react-redux';
+reactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
